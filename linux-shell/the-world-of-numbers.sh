108f161b2a1db76d64eb1279f6becbd7034d0dfd
matheusg18 <matheusgabriel.18@gmail.com>
2021-07-19 22:56:02 -0300

    The World of Numbers

---
+++ linux-shell/the-world-of-numbers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/problem?h_r=next-challenge&h_v=zen
+#
+read x
+read y
+echo $(($x + $y))
+echo $(($x - $y))
+echo $(($x * $y))
+echo $(($x / $y))
